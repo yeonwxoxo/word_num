d94b059725fd4955840c5781acc94705b16b2ba8
Kangyeonwoo <kangyeonwoo@Kangyeonwooui-MacBookPro.local>
2020-05-14 23:28:22 +0900

    단어개수과제

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+def count(request): 
+    return render(request, 'count.html')
+
+def result(request):
+    text = request.POST['text']
+    total_len = len(text)
+    no_blank_len = len(text.replace(" ",""))
+    word_num=len(text.split(' '))
+
+    return render(request, 'result.html',
+        {'total_len' : total_len,
+         'text': text,
+         'no_blank_len': no_blank_len,
+         'word_num':word_num,
+         
+         })
